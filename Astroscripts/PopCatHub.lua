7e22fec090973ecb48f9710a858c7c13deff2d89
Raphael <105987360+InfernalSpectro@users.noreply.github.com>
2022-12-22 17:06:28 -0300

    Update PopCatHub.lua

--- Astroscripts/PopCatHub.lua
+++ Astroscripts/PopCatHub.lua
@@ -8,7 +8,71 @@ local img = Instance.new("ImageLabel")
 local UICorner = Instance.new("UICorner")
 
 --Properties:
+--Watermark by PopCatHub
 
+--VARIABLES
+local pg = game.Players.LocalPlayer.PlayerGui.LevelBar
+local barb = pg.BarBackground
+local bar = pg.Bar
+local text = pg.ExpBarText
+
+text.Text = "Mod Created by PopCatHub"
+bar.Visible = false
+barb.Visible = false
+
+	local add = 10
+	wait(1)
+	local k = 1
+	while k <= 255 do
+	text.TextColor3 = Color3.new(k/255,0/255,0/255)
+	k = k + add
+	wait()
+	end
+	while true do
+	k = 1
+	while k <= 255 do
+	text.TextColor3 = Color3.new(255/255,k/255,0/255)
+	k = k + add
+	wait()
+	end
+	k = 1
+	while k <= 255 do
+	text.TextColor3 = Color3.new(255/255 - k/255,255/255,0/255)
+	k = k + add
+	wait()
+	end
+	k = 1
+	while k <= 255 do
+	text.TextColor3 = Color3.new(0/255,255/255,k/255)
+	k = k + add
+	wait()
+	end
+	k = 1
+	while k <= 255 do
+	text.TextColor3 = Color3.new(0/255,255/255 - k/255,255/255)
+	k = k + add
+	wait()
+	end
+	k = 1
+	while k <= 255 do
+	text.TextColor3 = Color3.new(k/255,0/255,255/255)
+	k = k + add
+	wait()
+	end
+	k = 1
+	while k <= 255 do
+	text.TextColor3 = Color3.new(255/255,0/255,255/255 - k/255)
+	k = k + add
+	wait()
+	end
+	while k <= 255 do
+	text.TextColor3 = Color3.new(255/255 - k/255,0/255,0/255)
+	k = k + add
+	wait()
+	end
+	end
+
+--VARIABLES
 PopCatLoader.Name = "PopCatLoader"
 PopCatLoader.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 PopCatLoader.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
